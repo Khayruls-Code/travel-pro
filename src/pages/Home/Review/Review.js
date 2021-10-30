bdbf21980d5be9f3c4f764d198691080331feb04
Khayrul Islam <khayrulislam2525@gmail.com>
2021-10-31 02:20:40 +0600

    update url

--- src/pages/Home/Review/Review.js
+++ src/pages/Home/Review/Review.js
@@ -4,7 +4,7 @@ import ReviewDetails from '../ReviewDetails/ReviewDetails';
 const Review = () => {
   const [reviews, setReviews] = useState([])
   useEffect(() => {
-    fetch('http://localhost:5000/review')
+    fetch('https://travel-pro-agency.herokuapp.com/review')
       .then(res => res.json())
       .then(data => setReviews(data))
   }, [])
